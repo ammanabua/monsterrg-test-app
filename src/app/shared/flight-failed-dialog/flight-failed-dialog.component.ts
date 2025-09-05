31be64515cd358d0d4c3040e8194be50d1abea80
Amman Abua <ammanabua@gmail.com>
2025-09-05 11:09:42 -0400

    fix close button bug

--- src/app/shared/flight-failed-dialog/flight-failed-dialog.component.ts
+++ src/app/shared/flight-failed-dialog/flight-failed-dialog.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-flight-failed-dialog',
-  imports: [],
   templateUrl: './flight-failed-dialog.component.html',
   styleUrl: './flight-failed-dialog.component.css'
 })
 export class FlightFailedDialogComponent {
-
+  constructor(private dialogRef: MatDialogRef<FlightFailedDialogComponent>) {}
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
--- src/app/shared/flight-success-dialog/flight-success-dialog.component.ts
+++ src/app/shared/flight-success-dialog/flight-success-dialog.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { MatDialog, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
-
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-flight-success-dialog',
-  imports: [],
   templateUrl: './flight-success-dialog.component.html',
   styleUrl: './flight-success-dialog.component.css'
 })
 export class FlightSuccessDialogComponent {
-
+  constructor(private dialogRef: MatDialogRef<FlightSuccessDialogComponent>) {}
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
